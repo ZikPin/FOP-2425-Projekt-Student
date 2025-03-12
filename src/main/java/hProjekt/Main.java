bc216210ef19fbc4a4b189823917c546a21664e9
Nazrin Murshudli <nazrin.murshudli7@gmail.com>
2025-03-12 20:43:26 +0400

    trying smth with branching

--- src/main/java/hProjekt/Main.java
+++ src/main/java/hProjekt/Main.java
@@ -20,6 +20,6 @@ public class Main {
 
         System.out.println("Ya realno samiy kruteyshiy");
 
-        System.out.println("пиздёж чистой воды! ");
+        //adding something for testbranch
     }
 }
