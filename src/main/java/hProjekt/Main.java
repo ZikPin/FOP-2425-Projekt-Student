d218dccbef388a35640d7e323758baddd6e77a2b
ZikPin <ziya.gulu365@gmail.com>
2025-03-12 15:31:40 +0100

    First commit

--- src/main/java/hProjekt/Main.java
+++ src/main/java/hProjekt/Main.java
@@ -13,5 +13,7 @@ public class Main {
      */
     public static void main(String[] args) {
         Application.launch(MyApplication.class, args);
+
+        System.out.println("Ya samiy krutoy");
     }
 }
